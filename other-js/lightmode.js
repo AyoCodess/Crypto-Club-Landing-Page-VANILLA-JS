8f1e01827a26e94fb09e7d74c663434c822007c2
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-02 13:15:07 +0200

    added new sectiob to PS area

--- other-js/lightmode.js
+++ other-js/lightmode.js
@@ -1,19 +1,16 @@
-//? on load
-
-document.documentElement.setAttribute(`data-theme`, `dark`);
-
 const toggleSwitch = document.getElementById(`toggle-icon`);
 const button = document.querySelector(`.theme-btn`);
 const buttonBg = document.querySelector(`.theme-btn-bg`);
 
 function lightMode() {
-  buttonBg.style.background = `linear-gradient(to right, #b9b9b9, #ffffff, #f6f5f4)`;
-  localStorage.setItem(`theme`, `dark`);
+  buttonBg.style.background = `linear-gradient(to right, #2c5364, #203a43, #0f2027)`;
+  localStorage.setItem(`theme`, `light`);
 }
 
 function darkMode() {
-  buttonBg.style.background = `linear-gradient(to right, #2c5364, #203a43, #0f2027)`;
-  localStorage.setItem(`theme`, `light`);
+  buttonBg.style.background = `linear-gradient(to right, #f6f5f4, #ffffff, #f6f5f4)`;
+  localStorage.setItem(`theme`, `dark`);
+  b9b9b9;
 }
 
 toggleSwitch.addEventListener(`click`, toggleTheme);
@@ -22,11 +19,13 @@ function toggleTheme(e) {
   if (button.innerHTML === `<i class="fas fa-moon"></i>`) {
     document.documentElement.setAttribute(`data-theme`, `dark`);
     button.innerHTML = `<i class="fas fa-sun"></i>`;
-    lightMode();
+    console.log(`dark mode`);
+    darkMode();
   } else {
     document.documentElement.removeAttribute(`data-theme`, `light`);
     button.innerHTML = `<i class="fas fa-moon"></i>`;
-    darkMode();
+    lightMode();
+    console.log(`light mode`);
   }
 }
 
@@ -34,9 +33,20 @@ function toggleTheme(e) {
 
 const currentTheme = localStorage.getItem(`theme`);
 
+if (currentTheme) {
+  document.documentElement.setAttribute(`data-theme`, currentTheme);
+}
+
 //? icon is not directly linked to theme, so needs to be changed manually
 
 if (document.documentElement.dataset.theme === "dark") {
   button.innerHTML = `<i class="fas fa-sun"></i>`;
+  darkMode();
+}
+
+if (document.documentElement.dataset.theme === "light") {
+  button.innerHTML = `<i class="fas fa-moon"></i>`;
   lightMode();
 }
+
+console.log(currentTheme);

fe5b041e29eb63c4a10c92266414e51a2ab1aadf
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-05 19:12:03 +0200

    fixed carousel speed bug

--- other-js/navbar.js
+++ other-js/navbar.js
@@ -4,9 +4,17 @@ const sideNav = document.querySelector(`.side-nav`);
 const sideNavList = document.getElementById(`nav`);
 const navMediaQuery = window.matchMedia("(max-width: 1215px)");
 const navMediaQuery2 = window.matchMedia("(min-width: 1215px)");
+const carouselInterval = document.getElementById(`newsAlerts`);
+
+function carouselDuration() {
+  if (navMediaQuery.matches) {
+    carouselInterval.dataset.bsInterval = `10`;
+  }
+
+  console.log(carouselInterval.dataset.bsInterval);
+}
 
 function hideSideBarOnScreenResize(e) {
-  console.log(e);
   if (navMediaQuery2.matches) {
     sideNav.classList.add(`hide`);
     sideNavList.classList.remove(`show`);
@@ -14,6 +22,7 @@ function hideSideBarOnScreenResize(e) {
 }
 
 window.addEventListener("resize", hideSideBarOnScreenResize);
+window.addEventListener("resize", carouselDuration);
 
 hideSideBarOnScreenResize();
 

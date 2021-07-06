461d7c5a6a25423d58fe172f59570fb8eaccb58b
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-06 17:20:02 +0200

    fixed duplicated navbar error on load

--- other-js/navbar.js
+++ other-js/navbar.js
@@ -1,6 +1,7 @@
 const hamburger = document.getElementById(`hamburger`);
 const theBody = document.body;
 const sideNav = document.querySelector(`.side-nav`);
+const navCon = document.querySelector(`.nav-con`);
 const sideNavList = document.getElementById(`side-nav`);
 const navMediaQuery = window.matchMedia("(max-width: 1215px)");
 const navMediaQuery2 = window.matchMedia("(min-width: 1215px)");
@@ -9,6 +10,7 @@ const carouselInterval = document.getElementById(`newsAlerts`);
 function hideSideBarOnScreenResize(e) {
   if (navMediaQuery2.matches) {
     sideNav.classList.add(`hide`);
+    navCon.classList.add(`hide`);
     sideNavList.classList.remove(`show`);
   }
 }
@@ -30,11 +32,13 @@ function mediaQuery() {
 }
 
 function toggleSideBar() {
-  if (sideNav.classList.contains(`hide`)) {
+  if (sideNav.classList.contains(`hide`) && navCon.classList.contains(`hide`)) {
     sideNav.classList.remove(`hide`);
+    navCon.classList.remove(`hide`);
     mediaQuery();
   } else {
     sideNav.classList.add(`hide`);
+    navCon.classList.add(`hide`);
     mediaQuery();
   }
 }
